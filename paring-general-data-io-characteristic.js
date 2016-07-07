b71ced73f70d81ff06bacc53f5fcf700504038e5
Anton Schegg <as5@schegg.de>
2016-07-07 19:15:43 +0200

    pairing challenge

--- paring-general-data-io-characteristic.js
+++ paring-general-data-io-characteristic.js
@@ -23,6 +23,8 @@ function ParingGeneralDataInputOutputCharacteristic(publicKey, privateKey) {
             this.privateKey = new Buffer(privateKey);
         }
     }
+    this.state = this.PAIRING_IDLE;
+
     ParingGeneralDataInputOutputCharacteristic.super_.call(this, {
         // uuid: 'a92ee101-5501-11e4-916c-0800200c9a66',
         uuid: 'a92ee101550111e4916c0800200c9a66',
@@ -37,6 +39,11 @@ function ParingGeneralDataInputOutputCharacteristic(publicKey, privateKey) {
 }
 
 util.inherits(ParingGeneralDataInputOutputCharacteristic, BlenoCharacteristic);
+
+ParingGeneralDataInputOutputCharacteristic.prototype.PAIRING_IDLE = 0;
+ParingGeneralDataInputOutputCharacteristic.prototype.PAIRING_SL_SEND_PUBKEY = 1;
+ParingGeneralDataInputOutputCharacteristic.prototype.PAIRING_CL_SEND_PUBKEY = 2;
+
 
 ParingGeneralDataInputOutputCharacteristic.prototype.getNextChunk = function (data) {
     var block0;
@@ -63,25 +70,32 @@ ParingGeneralDataInputOutputCharacteristic.prototype.onWriteRequest = function (
         } else if (data.length > 200) {
             callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
         } else {
-//            if (this.updateValueCallback) {
-                var cmd = new Buffer(2);
-                cmd.writeUInt16LE(nukiConstants.CMD_ID_PUBLIC_KEY);
-                var responseData = Buffer.concat([cmd, this.publicKey]);
-                var checksum = crc.crc16ccitt(responseData);
-                var checksumBuffer = new Buffer(2);
-                checksumBuffer.writeUInt16LE(checksum);
-            this.dataStillToSend = Buffer.concat([responseData, checksumBuffer]);
-                console.log("public key data with checksum:", this.dataStillToSend);
+            var rCmd = data.readUInt16LE(0);
+            var cmdId = data.readUInt16LE(2);
+            switch (this.state) {
+                case ParingGeneralDataInputOutputCharacteristic.prototype.PAIRING_IDLE:
+                    if (rCmd === nukiConstants.CMD_reqUEST_DATA && cmdId === nukiConstants.CMD_ID_PUBLIC_KEY) {
+                        var wCmd = new Buffer(2);
+                        wCmd.writeUInt16LE(nukiConstants.CMD_ID_PUBLIC_KEY);
+                        var responseData = Buffer.concat([wCmd, this.publicKey]);
+                        var checksum = crc.crc16ccitt(responseData);
+                        var checksumBuffer = new Buffer(2);
+                        checksumBuffer.writeUInt16LE(checksum);
+                        this.dataStillToSend = Buffer.concat([responseData, checksumBuffer]);
+                        console.log("prepared to send public key data with checksum:", this.dataStillToSend);
+                        this.state = ParingGeneralDataInputOutputCharacteristic.prototype.PAIRING_SL_SEND_PUBKEY = 1;
 
-                // var value = this.getNextChunk(this.dataStillToSend);
-                // if (value.length > 0) {
-                //     this.updateValueCallback(value);
-                // }
-                callback(this.RESULT_SUCCESS);
-//            } else {
-//                console.log("don't have updateValueCallback on write request");
-//                callback(this.RESULT_UNLIKELY_ERROR);
-//            }
+                        callback(this.RESULT_SUCCESS);
+                    }
+                    else {
+                        console.log("command or command identifier wrong");
+                        callback(this.RESULT_UNLIKELY_ERROR);
+                    }
+                    break;
+                default:
+                    console.log("ERROR unexpected pairing state");
+                    callback(this.RESULT_UNLIKELY_ERROR);
+            }
         }
     } else {
         console.log("checksum is NOT ok");
@@ -90,24 +104,32 @@ ParingGeneralDataInputOutputCharacteristic.prototype.onWriteRequest = function (
 };
 
 
-ParingGeneralDataInputOutputCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+ParingGeneralDataInputOutputCharacteristic.prototype.onSubscribe = function (maxValueSize, updateValueCallback) {
     console.log('ParingGeneralDataInputOutputCharacteristic - onSubscribe');
 
     this._updateValueCallback = updateValueCallback;
 
-    if (this.dataStillToSend.length > 0) {
-        var value = this.getNextChunk(this.dataStillToSend);
-        if (value.length > 0) {
-            console.log("sending " + value.length + " bytes from onSubscribe");
-            updateValueCallback(value);
-        }
-    } else {
-        console.log("don't have more data to indicate");
-    }
+    switch (this.state) {
+        case ParingGeneralDataInputOutputCharacteristic.prototype.PAIRING_SL_SEND_PUBKEY:
 
+            if (this.dataStillToSend.length > 0) {
+                var value = this.getNextChunk(this.dataStillToSend);
+                if (value.length > 0) {
+                    console.log("sending " + value.length + " bytes from onSubscribe");
+                    updateValueCallback(value);
+                }
+            } else {
+                console.log("don't have more data to indicate");
+            }
+
+            break;
+        default:
+            console.log("ERROR unexpected pairing state");
+            callback(this.RESULT_UNLIKELY_ERROR);
+    }
 };
 
-ParingGeneralDataInputOutputCharacteristic.prototype.onUnsubscribe = function() {
+ParingGeneralDataInputOutputCharacteristic.prototype.onUnsubscribe = function () {
     console.log('ParingGeneralDataInputOutputCharacteristic - onUnsubscribe');
 
     this._updateValueCallback = null;
