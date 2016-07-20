41a928c38b0a0886bab60978793030e8e8e8e1fe
Anton Schegg <as5@schegg.de>
2016-07-20 19:12:44 +0200

    advertise with eir data

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ bleno.on('stateChange', function (state) {
         var uuidBuf = new Buffer(keyturnerPairingService.uuid, 'hex');
         var serviceDataBuf = new Buffer('2000001B', 'hex');
         console.log("Advertise with EIR Data:", uuidBuf, serviceDataBuf);
-        bleno.startAdvertisingWithEIRData(uuidBuf, serviceDataBuf, function (err) {
+        bleno.startAdvertisingWithEIRData(serviceDataBuf, uuidBuf, function (err) {
             console.log("Advertising started", err);
         });
     } else {
